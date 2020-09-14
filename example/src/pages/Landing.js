479894f8b2582fbdbbc25e963562de960f3ff4c2
Kyle Soeltz <wsoeltz@gmail.com>
2020-09-14 17:14:06 -0400

    Fix link locations

--- example/src/pages/Landing.js
+++ example/src/pages/Landing.js
@@ -4,10 +4,10 @@ const App = () => {
 
   return (
     <ul>
-      <li><a href={'/#/original'}>Industry Space as <strong>Network</strong></a></li>
-      <li><a href={'/#/no-edges'}>Industry Space as <strong>Network w/o Edges</strong></a></li>
-      <li><a href={'/#/u-map'}>Industry Space as <strong>UMap</strong></a></li>
-      <li><a href={'/#/u-map-lines'}>Industry Space as <strong>UMap w/ Edges</strong></a></li>
+      <li><a href={'#/original'}>Industry Space as <strong>Network</strong></a></li>
+      <li><a href={'#/no-edges'}>Industry Space as <strong>Network w/o Edges</strong></a></li>
+      <li><a href={'#/u-map'}>Industry Space as <strong>UMap</strong></a></li>
+      <li><a href={'#/u-map-lines'}>Industry Space as <strong>UMap w/ Edges</strong></a></li>
     </ul>
   );
 }
