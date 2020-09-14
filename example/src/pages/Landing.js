39fff24f18ef2578ef8b0574af3ed42f4b64c4e6
Kyle Soeltz <wsoeltz@gmail.com>
2020-09-14 17:04:46 -0400

    Fix multipe renders of nodes

--- example/src/pages/Landing.js
+++ example/src/pages/Landing.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import {
-  Link,
-} from 'react-router-dom';
 
 const App = () => {
 
   return (
     <ul>
-      <li><Link to={'/original'}>Industry Space as <strong>Network</strong></Link></li>
-      <li><Link to={'/no-edges'}>Industry Space as <strong>Network w/o Edges</strong></Link></li>
-      <li><Link to={'/u-map'}>Industry Space as <strong>UMap</strong></Link></li>
-      <li><Link to={'/u-map-lines'}>Industry Space as <strong>UMap w/ Edges</strong></Link></li>
+      <li><a href={'/#/original'}>Industry Space as <strong>Network</strong></a></li>
+      <li><a href={'/#/no-edges'}>Industry Space as <strong>Network w/o Edges</strong></a></li>
+      <li><a href={'/#/u-map'}>Industry Space as <strong>UMap</strong></a></li>
+      <li><a href={'/#/u-map-lines'}>Industry Space as <strong>UMap w/ Edges</strong></a></li>
     </ul>
   );
 }
