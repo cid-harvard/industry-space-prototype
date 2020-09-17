371b5a5d70b28a3634bfcebbe598261564249fe6
Kyle Soeltz <wsoeltz@gmail.com>
2020-09-17 09:51:28 -0400

    Add UMAP with special edges data

--- example/src/App.js
+++ example/src/App.js
@@ -8,6 +8,8 @@ import IndustrySpaceNetwork from './pages/IndustrySpaceNetwork';
 import IndustrySpaceNetworkNoLines from './pages/IndustrySpaceNetworkNoLines';
 import IndustrySpaceUMap from './pages/IndustrySpaceUMap';
 import IndustrySpaceUMapWithLines from './pages/IndustrySpaceUMapWithLines';
+import IndustrySpaceUMapEdgesAbs from './pages/IndustrySpaceUMapEdgesAbs';
+import IndustrySpaceUMapEdgesThreshold from './pages/IndustrySpaceUMapEdgesThreshold';
 import Landing from './pages/Landing';
 
 const App = () => {
@@ -20,6 +22,8 @@ const App = () => {
             <Route exact path={'/no-edges'} component={IndustrySpaceNetworkNoLines} />
             <Route exact path={'/u-map'} component={IndustrySpaceUMap} />
             <Route exact path={'/u-map-lines'} component={IndustrySpaceUMapWithLines} />
+            <Route exact path={'/u-map-edges-abs'} component={IndustrySpaceUMapEdgesAbs} />
+            <Route exact path={'/u-map-edges-threshold'} component={IndustrySpaceUMapEdgesThreshold} />
             <Route component={Landing} />
           </Switch>
         </Router>
--- example/src/pages/Landing.js
+++ example/src/pages/Landing.js
@@ -8,6 +8,8 @@ const App = () => {
       <li><a href={'#/no-edges'}>Industry Space as <strong>Network w/o Edges</strong></a></li>
       <li><a href={'#/u-map'}>Industry Space as <strong>UMap</strong></a></li>
       <li><a href={'#/u-map-lines'}>Industry Space as <strong>UMap w/ Edges</strong></a></li>
+      <li><a href={'#/u-map-edges-abs'}>Industry Space as <strong>UMap w/ Edges, Highlighted Based on <em>Value</em></strong></a></li>
+      <li><a href={'#/u-map-edges-threshold'}>Industry Space as <strong>UMap w/ Edges, Highlighted based on <em>Threshold</em></strong></a></li>
     </ul>
   );
 }
