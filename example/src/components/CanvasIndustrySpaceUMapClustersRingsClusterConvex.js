4bc3d486916867200d8902328ec66f86cb6d63ec
Kyle Soeltz <wsoeltz@gmail.com>
2020-11-10 11:53:04 -0500

    Refine cluster shape and coloring

--- example/src/components/CanvasIndustrySpaceUMapClustersRingsClusterConvex.js
+++ example/src/components/CanvasIndustrySpaceUMapClustersRingsClusterConvex.js
@@ -43,69 +43,69 @@ function getLines(ctx, text, maxWidth) {
     return lines;
 }
 
-var roundedPoly = function(ctx, points,radius){
-    var i, x, y, len, p1, p2, p3, v1, v2, sinA, sinA90, radDirection, drawDirection, angle, halfAngle, cRadius, lenOut;
-    var asVec = function (p, pp, v) { // convert points to a line with len and normalised
-        v.x = pp.x - p.x; // x,y as vec
-        v.y = pp.y - p.y;
-        v.len = Math.sqrt(v.x * v.x + v.y * v.y); // length of vec
-        v.nx = v.x / v.len; // normalised
-        v.ny = v.y / v.len;
-        v.ang = Math.atan2(v.ny, v.nx); // direction of vec
-    }
-    v1 = {};
-    v2 = {};
-    len = points.length;                         // number points
-    p1 = points[len - 1];                        // start at end of path
-    for (i = 0; i < len; i++) {                  // do each corner
-        p2 = points[(i) % len];                  // the corner point that is being rounded
-        p3 = points[(i + 1) % len];
-        // get the corner as vectors out away from corner
-        asVec(p2, p1, v1);                       // vec back from corner point
-        asVec(p2, p3, v2);                       // vec forward from corner point
-        // get corners cross product (asin of angle)
-        sinA = v1.nx * v2.ny - v1.ny * v2.nx;    // cross product
-        // get cross product of first line and perpendicular second line
-        sinA90 = v1.nx * v2.nx - v1.ny * -v2.ny; // cross product to normal of line 2
-        angle = Math.asin(sinA);                 // get the angle
-        radDirection = 1;                        // may need to reverse the radius
-        drawDirection = false;                   // may need to draw the arc anticlockwise
-        // find the correct quadrant for circle center
-        if (sinA90 < 0) {
-            if (angle < 0) {
-                angle = Math.PI + angle; // add 180 to move us to the 3 quadrant
-            } else {
-                angle = Math.PI - angle; // move back into the 2nd quadrant
-                radDirection = -1;
-                drawDirection = true;
-            }
-        } else {
-            if (angle > 0) {
-                radDirection = -1;
-                drawDirection = true;
-            }
-        }
-        halfAngle = angle / 2;
-        // get distance from corner to point where round corner touches line
-        lenOut = Math.abs(Math.cos(halfAngle) * radius / Math.sin(halfAngle));
-        if (lenOut > Math.min(v1.len / 2, v2.len / 2)) { // fix if longer than half line length
-            lenOut = Math.min(v1.len / 2, v2.len / 2);
-            // ajust the radius of corner rounding to fit
-            cRadius = Math.abs(lenOut * Math.sin(halfAngle) / Math.cos(halfAngle));
-        } else {
-            cRadius = radius;
-        }
-        x = p2.x + v2.nx * lenOut; // move out from corner along second line to point where rounded circle touches
-        y = p2.y + v2.ny * lenOut;
-        x += -v2.ny * cRadius * radDirection; // move away from line to circle center
-        y += v2.nx * cRadius * radDirection;
-        // x,y is the rounded corner circle center
-        ctx.arc(x, y, cRadius, v1.ang + Math.PI / 2 * radDirection, v2.ang - Math.PI / 2 * radDirection, drawDirection); // draw the arc clockwise
-        p1 = p2;
-        p2 = p3;
-    }
-    ctx.closePath();
-}
+// var roundedPoly = function(ctx, points,radius){
+//     var i, x, y, len, p1, p2, p3, v1, v2, sinA, sinA90, radDirection, drawDirection, angle, halfAngle, cRadius, lenOut;
+//     var asVec = function (p, pp, v) { // convert points to a line with len and normalised
+//         v.x = pp.x - p.x; // x,y as vec
+//         v.y = pp.y - p.y;
+//         v.len = Math.sqrt(v.x * v.x + v.y * v.y); // length of vec
+//         v.nx = v.x / v.len; // normalised
+//         v.ny = v.y / v.len;
+//         v.ang = Math.atan2(v.ny, v.nx); // direction of vec
+//     }
+//     v1 = {};
+//     v2 = {};
+//     len = points.length;                         // number points
+//     p1 = points[len - 1];                        // start at end of path
+//     for (i = 0; i < len; i++) {                  // do each corner
+//         p2 = points[(i) % len];                  // the corner point that is being rounded
+//         p3 = points[(i + 1) % len];
+//         // get the corner as vectors out away from corner
+//         asVec(p2, p1, v1);                       // vec back from corner point
+//         asVec(p2, p3, v2);                       // vec forward from corner point
+//         // get corners cross product (asin of angle)
+//         sinA = v1.nx * v2.ny - v1.ny * v2.nx;    // cross product
+//         // get cross product of first line and perpendicular second line
+//         sinA90 = v1.nx * v2.nx - v1.ny * -v2.ny; // cross product to normal of line 2
+//         angle = Math.asin(sinA);                 // get the angle
+//         radDirection = 1;                        // may need to reverse the radius
+//         drawDirection = false;                   // may need to draw the arc anticlockwise
+//         // find the correct quadrant for circle center
+//         if (sinA90 < 0) {
+//             if (angle < 0) {
+//                 angle = Math.PI + angle; // add 180 to move us to the 3 quadrant
+//             } else {
+//                 angle = Math.PI - angle; // move back into the 2nd quadrant
+//                 radDirection = -1;
+//                 drawDirection = true;
+//             }
+//         } else {
+//             if (angle > 0) {
+//                 radDirection = -1;
+//                 drawDirection = true;
+//             }
+//         }
+//         halfAngle = angle / 2;
+//         // get distance from corner to point where round corner touches line
+//         lenOut = Math.abs(Math.cos(halfAngle) * radius / Math.sin(halfAngle));
+//         if (lenOut > Math.min(v1.len / 2, v2.len / 2)) { // fix if longer than half line length
+//             lenOut = Math.min(v1.len / 2, v2.len / 2);
+//             // ajust the radius of corner rounding to fit
+//             cRadius = Math.abs(lenOut * Math.sin(halfAngle) / Math.cos(halfAngle));
+//         } else {
+//             cRadius = radius;
+//         }
+//         x = p2.x + v2.nx * lenOut; // move out from corner along second line to point where rounded circle touches
+//         y = p2.y + v2.ny * lenOut;
+//         x += -v2.ny * cRadius * radDirection; // move away from line to circle center
+//         y += v2.nx * cRadius * radDirection;
+//         // x,y is the rounded corner circle center
+//         ctx.arc(x, y, cRadius, v1.ang + Math.PI / 2 * radDirection, v2.ang - Math.PI / 2 * radDirection, drawDirection); // draw the arc clockwise
+//         p1 = p2;
+//         p2 = p3;
+//     }
+//     ctx.closePath();
+// }
 
 const minExpectedScreenSize = 1020;
 
@@ -408,53 +408,56 @@ const createForceGraph = (rootEl, data, setNodeList, setHovered) => {
       context.translate(transform.x, transform.y);
       context.scale(transform.k, transform.k);
 
+
       // Draw the cluster shapes 
+      if (highlightedId === undefined) {
+        const convexMap = [
+          { id: '4', color: '#93CFD0', coords: [] },
+          { id: '5', color: '#488098', coords: [] },
+          { id: '9', color: '#77C898', coords: [] },
+          { id: '10', color: '#A973BE', coords: [] },
+          { id: '12', color: '#F1866C', coords: [] },
+          { id: '13', color: '#D35162', coords: [] },
+          { id: '14', color: '#FFC135', coords: [] },
+        ]
 
+        clusterMap.forEach(({naics, C1}) => {
+          const node = tempData.nodes.find(({id}) => id.toString() === naics.toString());
+          const convex = convexMap.find(({id}) => id.toString() === C1.toString());
+          if (node && convex) {
+            convex.coords.push([node.x, node.y]);
+          }
+        })
 
-      const convexMap = [
-        { id: '4', coords: [] },
-        { id: '5', coords: [] },
-        { id: '9', coords: [] },
-        { id: '10', coords: [] },
-        { id: '12', coords: [] },
-        { id: '13', coords: [] },
-        { id: '14', coords: [] },
-      ]
+        // const cornerRadius = 30;
+        context.lineWidth = 2;
+        convexMap.forEach(({coords, color}) => {
+          context.fillStyle = polished.rgba(color, 0.095);
+          context.strokeStyle = polished.rgba(color, 0.5);
+          context.beginPath(); // start a new path
 
-      clusterMap.forEach(({naics, C1}) => {
-        const node = tempData.nodes.find(({id}) => id.toString() === naics.toString());
-        const convex = convexMap.find(({id}) => id.toString() === C1.toString());
-        if (node && convex) {
-          convex.coords.push([node.x, node.y]);
-        }
-      })
+          // const points = hull(coords.map(([x, y]) => [xScale(x), yScale(y)]), 60)
+          //                 .map(([x, y]) => ({x, y}));
+          // roundedPoly(context, points, cornerRadius);
+          // context.fill();
+          // context.stroke();
 
-      context.fillStyle = '#efefef';
-      context.strokeStyle = '#efefef';
-      const cornerRadius = 30;
-      context.lineWidth = 6;
-      convexMap.forEach(({coords}) => {
-        context.beginPath(); // start a new path
-
-        const points = hull(coords.map(([x, y]) => [xScale(x), yScale(y)]), 60)
-                        .map(([x, y]) => ({x, y}));
-        roundedPoly(context, points, cornerRadius);
-        context.fill();
-        context.stroke();
-
-        // const points = hull(coords.map(([x, y]) => [xScale(x), yScale(y)]), 70)
-        // context.beginPath();
-        // context.lineCap = 'round'
-        // points.forEach((point, i) => {
-        //   if (i === 0) {
-        //     context.moveTo(...point);
-        //   } else {
-        //     context.lineTo(...point);
-        //   }
-        // })
-        // context.closePath();
-        // context.fill();
-      })
+          const points = hull(coords.map(([x, y]) => [xScale(x), yScale(y)]), 200)
+          context.beginPath();
+          context.lineCap = 'round'
+          points.forEach((point, i) => {
+            if (i === 0) {
+              context.moveTo(...point);
+            } else {
+              context.lineTo(...point);
+            }
+          })
+          context.closePath();
+          context.stroke();
+          context.fill();
+        })
+        context.lineWidth = 1;
+      } 
 
 
       // Draw the nodes
