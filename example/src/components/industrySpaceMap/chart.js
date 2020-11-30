bb35ac8784eec543c01ee87248a71c145015edf7
Kyle Soeltz <wsoeltz@gmail.com>
2020-11-30 15:32:06 -0500

    Add zoom to point

--- example/src/components/industrySpaceMap/chart.js
+++ example/src/components/industrySpaceMap/chart.js
@@ -8,6 +8,7 @@ export default (rootEl, data, rootWidth, rootHeight) => {
 
   const state = {
     zoom: 1,
+    active: null,
   };
 
   const svg = d3.select(rootEl).append("svg")
@@ -24,15 +25,6 @@ export default (rootEl, data, rootWidth, rootHeight) => {
 
   const g = svg.append("g");
 
-  g.selectAll(".industry-node")                  // plot a node at each data location
-    .data(data.nodes)
-    .enter().append("circle")
-      .attr("class", "industry-node")
-      .attr("cx", d => xScale(d.x) + margin.left )
-      .attr("cy", d => yScale(d.y) + margin.top )
-      .attr("r", d => d.radius)
-      .attr('fill', d => d.color)
-
   const zoom = d3.zoom()
     .scaleExtent([0.75, 50])
     .on("zoom", zoomed);
@@ -42,6 +34,72 @@ export default (rootEl, data, rootWidth, rootHeight) => {
   function zoomed() {
     state.zoom = d3.event.transform.k;
     g.attr("transform", d3.event.transform);
+    updateChart();
+  }
+
+  function reset() {
+    state.active = null;
+    svg.transition()
+      .duration(750)
+      .call(zoom.transform, d3.zoomIdentity);
+  }
+
+  g.selectAll(".industry-node")                  // plot a node at each data location
+    .data(data.nodes)
+    .enter().append("circle")
+      .attr("class", "industry-node")
+      .attr("cx", d => xScale(d.x) + margin.left )
+      .attr("cy", d => yScale(d.y) + margin.top )
+      .attr("r", d => d.radius)
+      .attr('fill', d => d.color)
+      .on("click", zoomToPoint);
+
+  function zoomToPoint(d) {
+    if (state.active !== null && state.active.node() === this) {
+      return reset();
+    }
+    if (state.active !== null) {
+      state.active.classed("active", false);
+    }
+    state.active = d3.select(this).classed("active", true);
+
+    const allEdgeXValues = [xScale(d.x) + margin.left];
+    const allEdgeYValues = [yScale(d.y) + margin.top];
+    // [...primaryNodes, ...secondaryNodes].forEach(n => {
+    //   allEdgeXValues.push(xScale(n.new_x));
+    //   allEdgeYValues.push(yScale(n.new_y));
+    // });
+
+    const xBounds = d3.extent(allEdgeXValues);
+    const yBounds = d3.extent(allEdgeYValues);
+    const bounds = [
+      [xBounds[0], yBounds[0]],
+      [xBounds[1], yBounds[1]],
+    ];
+    const dx = bounds[1][0] - bounds[0][0];
+    const dy = bounds[1][1] - bounds[0][1];
+    const x = (bounds[0][0] + bounds[1][0]) / 2;
+    const y = (bounds[0][1] + bounds[1][1]) / 2;
+    const scale = Math.max(1, Math.min(15, 0.9 / Math.max(dx / width, dy / height)));
+    const translate = [outerWidth / 2 - scale * x, outerHeight / 2 - scale * y];
+
+    svg.transition()
+      .duration(500)
+      .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale));
+
+  }
+
+  function updateChart() {
+    console.log(state);
+    if (state.active) {
+      g.selectAll(".industry-node")
+        .style('opacity', 0)
+      state.active
+        .style('opacity', 1)
+    } else {
+      g.selectAll(".industry-node")
+        .style('opacity', 1)
+    }
   }
 
 }
